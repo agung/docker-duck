5e276d97e1ea528710da3601358e8bb49ba68c92
Agung Nugraha <nugraha@192.168.1.5>
2021-10-09 12:03:33 +0700

    Add docker compose module for magento

---
+++ Console/Command/Install.php
@@ -0,0 +1,273 @@
+<?php
+
+namespace Duck\ComposeGenerate\Console\Command;
+
+use Duck\ComposeGenerate\Model\VersionInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Validation\ValidationException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class Install extends Command
+{
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * @var VersionInterface
+     */
+    protected $version;
+
+    /**
+     * InstallConstruct
+     *
+     * @param Filesystem $filesystem
+     * @param VersionInterface $version
+     */
+    public function __construct(Filesystem $filesystem, VersionInterface $version)
+    {
+        $this->filesystem = $filesystem;
+        $this->version = $version;
+        parent::__construct();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this->setName('duck:compose:install');
+        $this->setDescription('Generate docker compose');
+        $this->setDefinition($this->getOptionsList());
+        parent::configure();
+    }
+
+    /**
+     * Create interaction mode installation
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @throws ValidationException
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        /** @var QuestionHelper $questionHelper */
+        $questionHelper = $this->getHelper('question');
+        if (!$input->getOption('service-app')) {
+            $question = new Question('<question>Service App:</question> ', '');
+            $this->addNotEmptyValidator($question);
+
+            $input->setOption('service-app',
+                $questionHelper->ask($input, $output, $question)
+            );
+        }
+
+        if (!$input->getOption('magento-version')) {
+            $question = $this->gatherMagentoVersionWithSymfonyMenu($input, $output);
+            if (empty($question)) {
+                throw new ValidationException(__('The value cannot be empty'));
+            }
+
+            $input->setOption('magento-version', $question);
+        }
+
+        if (!$input->getOption('services')) {
+            $question = $this->gatherServicesWithSymfonyMenu($input, $output);
+            if (empty($question)) {
+                throw new ValidationException(__('The value cannot be empty'));
+            }
+
+            $input->setOption('services', $question);
+        }
+    }
+
+    /**
+     * CLI command description
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $this->buildDockerCompose($input->getOptions());
+        $this->buildDockerSync($input->getOptions());
+    }
+
+    /**
+     * Build the Docker compose file
+     *
+     * @param array $services
+     * @return void
+     */
+    protected function buildDockerCompose(array $services): void
+    {
+        // build depends on
+        $depends = array_filter($services['services'], function ($service) {
+            return in_array($service, ['mariadb', 'redis', 'elasticsearch']);
+        });
+        $depends = array_map(function ($service) {
+            return "            - {$service}";
+        }, $depends);
+        $depends = "depends_on:\n" . implode("\n", $depends);
+
+        // build services
+        $stubs = array_map(function ($service) {
+            return file_get_contents(__DIR__ . "/../../stubs/{$service}.stub");
+        }, $services['services']);
+        $stubs = rtrim(implode('', $stubs));
+
+        // build volumes
+        $volumes = array_filter($services['services'], function ($service) {
+            return in_array($service, ['mariadb']);
+        });
+        $volumes = array_map(function ($service) {
+            return "    duck{$service}:\n        driver: local";
+        }, $volumes);
+        if (!empty($volumes)) {
+            $volumes = "volumes:\n" . implode("\n", $volumes);
+        }
+
+        $dockerCompose = file_get_contents(__DIR__ . '/../../stubs/docker-compose.stub');
+
+        $dockerCompose = str_replace('{{depends}}', empty($depends) ? '' : '        ' . $depends, $dockerCompose);
+        $dockerCompose = str_replace('{{services}}', $stubs, $dockerCompose);
+
+        $magentoVersion = $this->version->version($services['magento-version']);
+        $php = $magentoVersion->getPhp();
+        $elasticsearch = $magentoVersion->getElasticsearch();
+        $mariadb = $magentoVersion->getMariadb();
+        $redis = $magentoVersion->getRedis();
+
+        array_push($services['services'], 'php');
+        foreach ($services['services'] as $service) {
+            $dockerCompose = str_replace("{{{$service}_version}}", ${$service}, $dockerCompose);
+        }
+        $dockerCompose = str_replace("{{service}}", $services['service-app'], $dockerCompose);
+        $dockerCompose = str_replace('{{volumes}}', $volumes, $dockerCompose);
+
+        // Remove empty lines...
+        $dockerCompose = preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $dockerCompose);
+
+        $root = $this->filesystem->getDirectoryRead(DirectoryList::ROOT)
+            ->getAbsolutePath('/');
+        file_put_contents($root . 'docker-compose.yml', $dockerCompose);
+    }
+
+    /**
+     * Build docker sync file
+     *
+     * @param array $services
+     */
+    protected function buildDockerSync(array $services)
+    {
+        $stub = file_get_contents(__DIR__ . "/../../stubs/docker-sync.stub");
+        $dockerSync = str_replace("{{service}}", $services['service-app'], $stub);
+
+        $root = $this->filesystem->getDirectoryRead(DirectoryList::ROOT)
+            ->getAbsolutePath('/');
+        file_put_contents($root . 'docker-sync.yml', $dockerSync);
+    }
+
+    /**
+     * Gather the desired duck magento version using a Symfony menu.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return string
+     */
+    protected function gatherMagentoVersionWithSymfonyMenu(
+        InputInterface $input,
+        OutputInterface $output
+    ): string {
+        $question = new ChoiceQuestion(
+            'Which magento version would you like to install?',
+            $this->version->getList(),
+            0
+        );
+        $question->setMaxAttempts(null);
+        return (new SymfonyStyle($input, $output))
+            ->askQuestion($question);
+    }
+
+    /**
+     * Gather the desired duck services using a Symfony menu.
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return array
+     */
+    protected function gatherServicesWithSymfonyMenu(
+        InputInterface $input,
+        OutputInterface $output
+    ): array {
+        $question = new ChoiceQuestion(
+            'Which services would you like to install?',
+            ['mariadb', 'redis', 'elasticsearch'],
+            0
+        );
+        $question->setMaxAttempts(null)
+            ->setMultiselect(true);
+        return (new SymfonyStyle($input, $output))
+            ->askQuestion($question);
+    }
+
+    /**
+     * Get list of arguments for the command
+     *
+     * @param int $mode The mode of options.
+     * @return InputOption[]
+     */
+    protected function getOptionsList(int $mode = InputOption::VALUE_REQUIRED): array
+    {
+        $required = ($mode === InputOption::VALUE_REQUIRED)
+            ? '(Required) '
+            : '';
+
+        return [
+            new InputOption(
+                'service-app',
+                null,
+                $mode,
+                $required . 'Service App'
+            ),
+            new InputOption(
+                'magento-version',
+                null,
+                $mode,
+                $required . 'Magento Version'
+            ),
+            new InputOption(
+                'services',
+                null,
+                $mode,
+                $required . 'Services'
+            ),
+        ];
+    }
+
+    /**
+     * Validate not empty question
+     *
+     * @param Question $question
+     */
+    private function addNotEmptyValidator(Question $question)
+    {
+        $question->setValidator(function ($value) {
+            if (trim($value) == '') {
+                throw new ValidationException(__('The value cannot be empty'));
+            }
+            return $value;
+        });
+    }
+}
---
+++ Model/Version.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Duck\ComposeGenerate\Model;
+
+class Version implements VersionInterface
+{
+    protected string $elasticsearch;
+    protected string $mariadb;
+    protected string $redis;
+    protected string $php;
+
+    /**
+     * Magento version
+     *
+     * @param string $version
+     * @return self
+     */
+    public function version(string $version): self
+    {
+        // elasticsearch version 6 not supported for arm
+        switch ($version) {
+            case '2.3.4':
+                $this->elasticsearch = '6.8.18';
+                $this->mariadb = '10.2';
+                $this->redis = '5.0';
+                $this->php = '7.2';
+                break;
+            case '2.4.0':
+                $this->elasticsearch = '7.6.2';
+                $this->mariadb = '10.2';
+                $this->redis = '5.0';
+                $this->php = '7.4';
+                break;
+            case '2.4.2':
+            case '2.4.3':
+                $this->elasticsearch = '7.9.0';
+                $this->mariadb = '10.4';
+                $this->redis = '6.0';
+                $this->php = '7.4';
+                break;
+            default:
+                break;
+        }
+        return $this;
+    }
+
+    /**
+     * Get list magento version
+     *
+     * @return array
+     */
+    public function getList(): array
+    {
+        return [
+            '2.3.4',
+            '2.4.0',
+            '2.4.2',
+            '2.4.3'
+        ];
+    }
+
+    /**
+     * Get elasticsearch version
+     *
+     * @return string
+     */
+    public function getElasticsearch(): string
+    {
+        return $this->elasticsearch;
+    }
+
+    /**
+     * Get elasticsearch version
+     *
+     * @param string $elasticsearch
+     * @return void
+     */
+    public function setElasticsearch(string $elasticsearch): void
+    {
+        $this->elasticsearch = $elasticsearch;
+    }
+
+    /**
+     * Get mariadb version
+     *
+     * @return string
+     */
+    public function getMariadb(): string
+    {
+        return $this->mariadb;
+    }
+
+    /**
+     * Set mariadb version
+     *
+     * @param string $mariadb
+     */
+    public function setMariadb(string $mariadb): void
+    {
+        $this->mariadb = $mariadb;
+    }
+
+    /**
+     * Get redis version
+     *
+     * @return string
+     */
+    public function getRedis(): string
+    {
+        return $this->redis;
+    }
+
+    /**
+     * Set redis version
+     *
+     * @param string $redis
+     */
+    public function setRedis(string $redis): void
+    {
+        $this->redis = $redis;
+    }
+
+    /**
+     * Get php version
+     *
+     * @return string
+     */
+    public function getPhp(): string
+    {
+        return $this->php;
+    }
+
+    /**
+     * Set php version
+     *
+     * @param string $php
+     */
+    public function setPhp(string $php): void
+    {
+        $this->php = $php;
+    }
+}
